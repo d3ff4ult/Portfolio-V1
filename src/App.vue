6948a50513378db9b45c80c4a80a03c4156c69a3
Eng.Abdelrhman <abdoh6112113@gmail.com>
2025-07-20 09:42:38 +0300

    Updated file with new changes

--- src/App.vue
+++ src/App.vue
@@ -37,10 +37,10 @@
           aria-label="Toggle mobile menu"
         >
           <svg v-if="isMobileMenuOpen" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
           <svg v-else class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
       </div>
@@ -78,7 +78,7 @@
       v-if="isMobileMenuOpen"
       @click="closeMobileMenu"
       class="fixed inset-0 z-40 bg-black bg-opacity-50 md:hidden"
-    />
+    ></div>
   </nav>
 </template>
 
